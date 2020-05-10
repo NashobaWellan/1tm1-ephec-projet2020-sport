7032826541b214beff497cae825ab3d95746b1c5
Nicolas-De-Weerd <64020422+Nicolas-De-Weerd@users.noreply.github.com>
2020-05-10 19:24:16 +0200

    Add files via upload

---
+++ backend/scripts-api-rest/HE201431-Nicolas-De-Weerd-webservice-joueur.sql
@@ -0,0 +1,23 @@
+CREATE PROCEDURE "DBA"."getMontreSportif"()
+result (id char(255),sportif char(30))
+BEGIN
+ 	select joueurId, joueur
+ 	from tbjoueur
+END;
+
+CREATE PROCEDURE "DBA"."getTrouve"(in sportif int)
+
+BEGIN 
+	Select tbjoueur.joueur, tbtournoi.tournoi, tbsport.sport, annee
+	from tbannee 
+	join tbjoueur on tbannee.joueurId = tbjoueur.joueurId
+	join tbtournoi on tbannee.tournoiId = tbtournoi.tournoiId
+	join tbsport on tbtournoi.sportId = tbsport.sportId
+	where tbannee.joueurId = sportif
+	order by annee
+END;
+
+
+CREATE SERVICE "joueur" TYPE 'JSON' AUTHORIZATION OFF USER "DBA" METHODS 'GET' AS call "dba"."getMontreSportif"();
+
+CREATE SERVICE "getTrouveSportif" TYPE 'JSON' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call".dba"."getTrouve"(:sportif);
