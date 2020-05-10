d5d0c85ecdb6ad3dd8dbedb36845cd0ec655a0ef
de-Pret-Mikael <64020511+de-Pret-Mikael@users.noreply.github.com>
2020-05-10 23:54:57 +0200

    Update and rename he201775-Mikael-de-Pret-webservice-modif.js to projet2.js

---
+++ frontend/js/projet2.js
@@ -0,0 +1,53 @@
+"use strict"
+
+//Projet Database
+//------------------
+// 1TM1 / de Pret / Mikaël / HE201775
+/*
+function showHint(str) {
+	var selectedValue = selectBox.text[selectBox.selectedIndex].value;
+  if (str.length == 0) {
+    document.getElementById("txtHint").innerHTML = "";
+    return;
+  } else {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("txtHint").innerHTML = this.responseText;
+      }
+    };
+    xmlhttp.open("GET", "Find?sportif=" + str, true);
+    xmlhttp.send();
+  }
+}
+*/
+function creerTableau1(rep, id){
+	let ligne1 = '';
+	for (let a of rep) {
+		ligne1 += '<option value=' + a.id + '>' + a.sportif + '</option>' + '\n';
+	}
+	document.getElementById(id).innerHTML = ligne1;
+}
+
+function modifier(){
+	var selectBox1 = document.getElementById("modif");
+    var selectedValue1 = selectBox1.options[selectBox1.selectedIndex].value;
+	var selectBox2 = document.getElementById("newModif");
+    var selectedValue2 = selectBox2.options[selectBox2.selectedIndex].value;
+    console.log(selectedValue1);
+	console.log(selectedValue2);
+	let xhr = new XMLHttpRequest();
+	// xhr.alterr;
+	xhr.open('put', 'alterr?sportif=?' + selectedValue1 + '?sportifNew=' + selectedValue2, true);
+	// EXEC SQL PUT tbjoueur FROM :joueurID, :Sportif;
+	// PUT http[s]://<host:88>/[public/]odata/applications/{v1|latest}/{appid}/Connections(‘{registrationID}’)
+    // xhr.open('put', 'alterr?sportif=&newSportif=', true); // préparer 
+	
+    xhr.onload = // callback : fonction anonyme
+        function(){
+            JSON.stringify(this.responseText);
+			// //console.log(this.responseText);
+         } 
+    xhr.send();
+}
+
