8788394fcf2babafcab00578e7b8ffd4d56bd8d7
de-Pret-Mikael <64020511+de-Pret-Mikael@users.noreply.github.com>
2020-05-10 14:54:17 +0200

    Update creation-tables.sql

--- backend/scripts-base-de-donnees/creation-tables.sql
+++ backend/scripts-base-de-donnees/creation-tables.sql
@@ -14,14 +14,14 @@ constraint pk_tbsport PRIMARY KEY (sportId),
 create table tbtournoi ( 
         tournoiId INTEGER NOT NULL DEFAULT AUTOINCREMENT,
         tournoi char(30) not null,
-	sportId char(30) not null,
+	sportId char(255) not null,
 CONSTRAINT pk_tbtournoi PRIMARY KEY (tournoiId),
 constraint fk_tbsport_tbtournoi foreign key (sport_sportId) references tbsport (sportId)
 );
 
 create table tbannee (
-        tournoiId char(2) not null,
-        joueurId char(2) not null,
+        tournoiId char(255) not null,
+        joueurId char(255) not null,
         annee char(4) not null,
 CONSTRAINT pk_tbannee PRIMARY KEY (tournoiId, joueurId),
 constraint fk_tbannee_tbjoueur foreign  key (joueur_joueurId) reference tbjoueur (joueurId),
