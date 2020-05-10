b1ab26f5555bd0babc746dd8d7b460666ede94d8
de-Pret-Mikael <64020511+de-Pret-Mikael@users.noreply.github.com>
2020-05-10 23:48:33 +0200

    Update he201775-Mikael-de-Pret-webservice-modif.sql

--- backend/scripts-api-rest/he201775-Mikael-de-Pret-webservice-modif.sql
+++ backend/scripts-api-rest/he201775-Mikael-de-Pret-webservice-modif.sql
@@ -1,21 +1,17 @@
 CREATE  PROCEDURE "DBA"."putAlterartion"(in sportif char, sportifNew char )
 BEGIN 
-    DECLARE  sportifid INT;
-    SET  sportifid = (SELECT joueurId
-        FROM tbjoueur
-    where joueur = sportif);
-	UPDATE dba.tbjoueur
+  UPDATE dba.tbjoueur
      SET  joueur = sportifNew
-     WHERE  joueurId = sportifid;
+     where tbjoueur.joueurId = sportif
 END 
 
 CREATE SERVICE "alterr" TYPE 'JSON' AUTHORIZATION OFF USER "DBA" METHODS 'PUT' AS call "DBA"."putAlterartion"(:sportif, :sportifNew);
 
-CREATE PROCEDURE "DBA"."getFind"(in sportif char)
-BEGIN
-    SELECT joueur
-        FROM tbjoueur
-        WHERE joueur LIKE '%a'
-END
+// CREATE PROCEDURE "DBA"."getFind"(in sportif char)
+// BEGIN
+  //  SELECT joueur
+    //    FROM tbjoueur
+      //  WHERE joueur LIKE '%a'
+// END
 
-CREATE SERVICE "find" TYPE 'JSON' AUTHORIZATION OFF USER "DBA" METHODS 'GET' AS call "DBA"."getFind"();
+// CREATE SERVICE "find" TYPE 'JSON' AUTHORIZATION OFF USER "DBA" METHODS 'GET' AS call "DBA"."getFind"();
