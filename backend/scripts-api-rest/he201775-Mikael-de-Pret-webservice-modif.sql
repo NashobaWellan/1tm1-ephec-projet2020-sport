254de395ec99809cafd83e1e18b3ecbf86793b74
de-Pret-Mikael <64020511+de-Pret-Mikael@users.noreply.github.com>
2020-05-19 09:28:16 +0200

    Update he201775-Mikael-de-Pret-webservice-modif.sql

--- backend/scripts-api-rest/he201775-Mikael-de-Pret-webservice-modif.sql
+++ backend/scripts-api-rest/he201775-Mikael-de-Pret-webservice-modif.sql
@@ -7,5 +7,5 @@ BEGIN
 
 END
 
-CREATE SERVICE "alterr" TYPE 'JSON' AUTHORIZATION OFF USER "DBA" METHODS 'PUT' AS call "DBA"."putAlterartion"(:sportifN,:sportifNewN,:sportifP,:sportifNewP);
+CREATE SERVICE "alterr" TYPE 'JSON' AUTHORIZATION OFF USER "DBA" METHODS 'GET' AS call "DBA"."putAlterartion"(:sportifN,:sportifNewN,:sportifP,:sportifNewP);
 
